6f08f1b983d83bb921ec4055040716b05ca4a80a
andrew dehel <dehel.andrew@gmail.com>
2018-07-19 11:24:45 -0400

    Change how files are run
    new file:   gbn_input.txt
    deleted:    input.txt
    modified:   run.sh
    new file:   stop_wait_input.txt

--- 2-transport/src/run.sh
+++ 2-transport/src/run.sh
@@ -1,18 +1,18 @@
 #Stop and Wait
 echo "-----STOP AND WAIT-----"
-gcc -w project2_stop_wait.c -o stop_wait
+gcc project2_stop_wait.c -o stop_wait
 if [[ $? -ne 0 ]]; then
-    exit
+    echo "project2_stop_wait.c did not compile"
 else
-    ./stop_wait < input.txt
+    ./stop_wait < stop_wait_input.txt > stop_wait_output.txt
 fi
 echo -e "\n\n\n"
 echo "-----GO BACK N-----"
-#Go Back N
 gcc project2_gbn.c -o gbn
 if [[ $? -ne 0 ]]; then
-    exit
+    echo "project2_gbn.c did not compile"
 else
-    ./gbn < input.txt
+    ./gbn < gbn_input.txt > gbn_output.txt
 fi
-
+echo -e "\n\n\n"
+#Go Back N
